27ff631a19a852de651fcf2a413659787e761bf9
Nikita Reddy <nikitareddy@172-18-11-3.DYNAPOOL.NYU.EDU>
2017-09-26 23:55:54 -0400

    Commit this

---
+++ scripts.js
@@ -0,0 +1,7 @@
+let tipCalculator;
+tipCalculator = function(total, tipRate){
+let tipAmount;
+tipAmount = tipRate * total;
+$("#response").html("Your tip is $" + tipAmount);
+}; 
+tipCalculator(50.00, 0.2);
