e3d2d5241935701271dfeb68db3938234e370cdc
Nikita Reddy <nikitareddy@172-18-14-7.DYNAPOOL.NYU.EDU>
2017-10-03 23:11:04 -0400

    hw 5 question 1

--- scripts.js
+++ scripts.js
@@ -1,8 +1,5 @@
-let tipCalculator;
-tipCalculator = function(total, rate){
-  let tipAmount;
-  let tipRate = rate/100;
-  tipAmount = tipRate * total;
-  $("#response").html("Your tip is $" + tipAmount);
-};
-tipCalculator(50.00, 20);
+let arrayOfStrings, arrayOfNumbers, arrayMixed;
+arrayOfStrings = ["a", "b", "c", "d", "e"];
+arrayOfNumbers = [1, 2, 3];
+arrayMixed = ["a", 1, null, true, arrayOfNumbers, [4.5, 5.6]];
+$("#response").html(arrayOfStrings[arrayOfStrings.length - 1]);
