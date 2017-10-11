b87b7628b6e3ac4fcf9d6f35317ecc90173a1133
Nikita Reddy <nikitareddy@172-18-42-143.DYNAPOOL.NYU.EDU>
2017-10-10 21:30:59 -0400

    Number 2 commit

--- scripts.js
+++ scripts.js
@@ -1,3 +1,10 @@
-let number;
-number = {first: "2", second: "4", third: "6"};
-$("#response").html(first, second, third);
+let leonardo, donatello, raphael, michelangelo, turtles, weapons;
+leonardo = {name: "Leonardo", color: "blue", weapon: "katana"};
+donatello = {name: "Donatello", color: "purple", weapon: "bo"};
+raphael = {name: "Raphael", color: "red", weapon: "sai"};
+michelangelo = {name: "Michelangelo", color: "blue", weapon: "nunchaku"};
+turtles = [leonardo, donatello, raphael, michelangelo];
+weapons = turtles.map(function(turtle){
+  return turtle.weapon;
+}).sort();
+$("#response").html(weapons);
