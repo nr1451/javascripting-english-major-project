e92634ffeea84de976036605fa9798269e9b5ed9
Nikita Reddy <nikitareddy@172-18-43-89.DYNAPOOL.NYU.EDU>
2017-11-14 22:29:41 -0500

    Commit this.

--- leaflet.js
+++ leaflet.js
@@ -8,7 +8,15 @@ L.tileLayer("https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x
     });
     tileLayer.addTo(firstMap);
     firstMap.setView([40.730833, -73.9975], 16);
-let bobstLibrary;
-bobstLibrary = L.marker([40.729444, -73.997222]);
-bobstLibrary.addTo(firstMap);
-bobstLibrary.bindPopup("This is Bobst Library.");
+let washingtonSquarePark, washingtonSquareParkMarker;
+let tenThousandth, tenThousandthPolygon, thousandth, thousandthPolyline;
+washingtonSquarePark = L.latLng(40.730833, -73.9975);
+washingtonSquareParkMarker = L.marker(washingtonSquarePark).addTo(firstMap);
+tenThousandth = [[40.7307, -73.9976], [40.7307, -73.9974],
+                 [40.7309, -73.9974], [40.7309, -73.9976]];
+    circle = L.circle(washingtonSquarePark, {radius: 100,
+    color: "#859900",
+    fillColor: "#cb4b16",
+    opacity: 0.9,
+    fillOpacity: 0.25}
+              ).addTo(firstMap);
