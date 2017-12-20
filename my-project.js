762fa78eab5a6b7eb1e9dbc99b3bfdad4780790b
Nikita Reddy <nikitareddy@172-17-9-247.DYNAPOOL.NYU.EDU>
2017-12-20 11:00:35 -0500

    Commit this.

--- my-project.js
+++ my-project.js
@@ -8,15 +8,15 @@ L.tileLayer("https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x
     });
     tileLayer.addTo(firstMap);
     firstMap.setView([40.762073, -73.981082], 12);
-let southEnd, southEndMarker;
+let southEnd, southEndMarker, popupContent;
     southEnd = L.latLng(40.710489, -74.016228);
     southEndMarker = L.marker(southEnd).addTo(firstMap).bindPopup("This is South End Avenue");
 let harlem125, harlem125Marker;
   harlem125 = L.latLng(40.805144, -73.939180);
-  harlem125Marker = L.marker(harlem125).addTo(firstMap).bindPopup("This is Harlem 125th Street");
+  harlem125Marker = L.marker(harlem125).addTo(firstMap).bindPopup("This is Harlem 125th St");
 let harlem124, harlem124Marker;
   harlem124 = L.latLng(40.806685, -73.945745);
-  harlem124Marker = L.marker(harlem124).addTo(firstMap).bindPopup("This is Harlem 124th Street");
+  harlem124Marker = L.marker(harlem124).addTo(firstMap).bindPopup("This is 124th Street");
 let cabride, cabrideMarker;
   cabride = L.latLng(40.773128, -73.981604);
   cabrideMarker = L.marker(cabride).addTo(firstMap).bindPopup("This is the American Folk Art Museum");
@@ -52,7 +52,7 @@ let md;
         });
 let md3;
         md3 = window.markdownit({html: true}).use(window.markdownitFootnote);
-        ["124-street"].forEach(function(tab){
+        ["harlem-124"].forEach(function(tab){
           // Create a variable tab that has the name as a string.
           $.ajax({
             // tab + ".md" yields, for example, "rampart.md".
